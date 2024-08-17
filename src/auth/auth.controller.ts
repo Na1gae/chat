9789402762bc7fa8ac989b1c7502abe1c4e5ffee
Na1gae <nalgae@korea.ac.kr>
2024-08-17 23:57:03 +0900

    Refactor: JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,8 @@
 import { Controller, Get, Redirect, Req, Res, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService){}
 
-    @Get('naver')
-    @UseGuards(AuthGuard('naver'))
-    async naverAuth(@Req() req: Request){
-        
-    }
-
-    @Get('naver/callback')
-    @UseGuards(AuthGuard('naver'))
-    async naverAuthCallback(@Req() req: Request, @Res() res: Response): Promise<any>{
-        return ;
-    }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-    controllers: [AuthController],
-    providers: [AuthService]
+    imports: [
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigModule],
+            useFactory: async(configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET_CODE'),
+                signOptions: {expiresIn: '100000'}
+            })
+        })
+    ],
+    providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
