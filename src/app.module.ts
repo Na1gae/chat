9dac4bed5384440f6140eea0639b435ff01e48c7
Na1gae <nalgae@korea.ac.kr>
2024-08-17 23:56:20 +0900

    Feat: 파일 업로드 / JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { FileModule } from './file/file.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, ChatModule],
+  imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env',
+      }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+    })}), AuthModule, ChatModule, FileModule],
   controllers: [AppController],
-  providers: [],
+  providers: []
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    async validateUser(user: {}): Promise<any>{
-        
+    constructor(private readonly jwtService: JwtService){}
+
+    async generateJWT(user: string): Promise<string>{ //추후 user는 UserInterface 적용
+        return this.jwtService.signAsync({user})
+    }
+
+    async passwordHash(password: string): Promise<string>{
+        return 
+        //bcrypt or SHA
+    }
+
+    async comparePassword(password: string, hashedPassword: string): Promise<any>{
+    
+    }
+
+    verifyJwt(jwt: string): Promise<any>{
+        return this.jwtService.verifyAsync(jwt)
     }
 }
