9b413315eee6bd0f2780ef14d05ab1d300b8d930
Na1gae <nalgae@korea.ac.kr>
2024-08-18 23:27:02 +0900

    Refactor: 데이터베이스 연결을 위해 코드 재작성

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,42 +1,27 @@
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { AuthService } from 'src/auth/auth.service';
-import { MessageInterface } from './model/message/message.interface';
+import { MessageInterface } from './model/message.interface';
+import { UseGuards } from '@nestjs/common';
+import { WsJwtAuthGuard } from 'src/auth/guard/ws-jwt-auth.guard';
+import { UserService } from 'src/user/user.service';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+  origin: '*'
+}})
+
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect{
-  @WebSocketServer() //CORS
+  @WebSocketServer()
   server : Server
 
   constructor(
-    private authService: AuthService
+    private authService: UserService
   ){}
 
-  clients: {[socketId: string]: boolean} = {}
-  clientNick: {[socketId: string]: string} = {}
-  roomUsers: {[key:string]: string[]} = {}
+  handleConnection(client: Socket){}
+  handleDisconnect(client: Socket){}
 
-  handleConnection(client: Socket): void{
-    //JWT 검증 추가
-    if(this.clients[client.id]){
-      client.disconnect(true)
-    }
-    this.clients[client.id] = true
-  }
-
-  handleDisconnect(client: Socket) {
-    //다른 서비스로 뺄 예정
-    delete this.clients[client.id]
-
-    Object.keys(this.roomUsers).forEach((r) =>{{
-      const idx = this.roomUsers[r]?.indexOf(this.clientNick[client.id])
-      if (idx !== -1){
-        this.roomUsers[r].splice(idx, 1)
-        this.server.to(r).emit('disconnect', {userNick: this.clientNick[client.id],userId: client.id, r})
-      }
-    }})
-  }
-  
+  @UseGuards(WsJwtAuthGuard)
   @SubscribeMessage('chat')
     async addMessage(socket: Socket, message: MessageInterface){
       //const createdMessage: MessageInterface = await this.messageService.create({...message, user: })
@@ -46,32 +31,3 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect{
       }*/
     }
 }
-
-/*
-  @SubscribeMessage('join')
-  handleJoin(client: Socket, room: string){
-    if(client.rooms.has(room)) return
-
-    client.join(room)
-    if(!this.roomUsers[room]){
-      this.roomUsers[room] = []
-    }
-
-    this.roomUsers[room].push(this.clientNick[client.id])
-    this.server.to(room).emit('join', {userNick: this.clientNick[client.id],userId: client.id, room})
-    this.server.to(room).emit('userList', {room, userList: this.roomUsers[room]})
-  }
-
-  @SubscribeMessage('exit')
-  handleExit(client: Socket, room: string){
-    if(client.rooms.has(room)) return
-    
-    client.leave(room)
-
-    const idx = this.roomUsers[room]?.indexOf(this.clientNick[client.id])
-      if (idx !== -1){
-        this.roomUsers[room].splice(idx, 1)
-        this.server.to(room).emit('left', {userNick: this.clientNick[client.id], userId: client.id, room})
-        this.server.to(room).emit('userList', {room, userList: this.roomUsers[room]})
-      }
-  } */
