0e00299ebf9c87348453754dc4d7df1724a23f0e
Na1gae <nalgae@korea.ac.kr>
2024-08-20 02:02:23 +0900

    Feat: 내부 로직 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Headers } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    
+    constructor(
+        private readonly userService: UserService
+    ){}
+
+    @Get('/idJungbok')
+    async idjungbok(@Body('id') id: string){ //횟수 카운트 추가
+        return this.userService.idJungbok(id)
+    }
+
+    @Get('/profileimg')
+    async getProfileimgByid(@Body('id') id: string){ //JWT Permission Check 추가
+        return this.userService.profileimg(id)
+    }
+
+    @Get('/getChatrooms')
+    async getChatroomsByuserId(userId: string){ //userId -> JWT
+        return this.userService.getUserChatrooms(userId)
+    }
+
+    @Get('/getChats')
+    async getChatByRoomId(@Body('id') roomId: string, userId :string ){ //userId -> JWT
+        return this.userService.getChatsByRoomId(userId, roomId)
+    }
 }
