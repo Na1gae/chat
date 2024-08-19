ba68ab32c6e256de2ef694fffa3c2fd78d395d1c
Na1gae <nalgae@korea.ac.kr>
2024-08-20 01:54:47 +0900

    Fix: 오타

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,10 @@
-import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MessageInterface } from './model/message.interface';
 import { UseGuards } from '@nestjs/common';
 import { WsJwtAuthGuard } from 'src/auth/guard/ws-jwt-auth.guard';
 import { UserService } from 'src/user/user.service';
+import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway({
   cors: {
@@ -15,15 +16,19 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect{
   server : Server
 
   constructor(
-    private authService: UserService
+    private authService: AuthService
   ){}
 
-  handleConnection(client: Socket){}
-  handleDisconnect(client: Socket){}
+  handleConnection(client: Socket){
+    console.log(`Connected: ${client}`)
+  }
+  handleDisconnect(client: Socket){
+    console.log(`Disconnected: ${client}`)
+  }
 
   @UseGuards(WsJwtAuthGuard)
   @SubscribeMessage('chat')
-    async addMessage(socket: Socket, message: MessageInterface){
+    async addMessage(socket: Socket, @MessageBody() message: MessageInterface){
       //const createdMessage: MessageInterface = await this.messageService.create({...message, user: })
       //const joinedUsers: JoinedRoomInterface[] = await this.joinedRoomService.findByRoom(room)
       /*for(const user of joinedUsers){
