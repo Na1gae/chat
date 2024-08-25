6a1138a62d33f134d31ac98cd8235440b2eb6440
Na1gae <nalgae@korea.ac.kr>
2024-08-25 22:22:06 +0900

    Feat: DataWrap Interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class DataWrapInterceptor implements NestInterceptor{
+  async intercept(context: ExecutionContext, next: CallHandler): Promise<any>{
+    try{
+      const res = await lastValueFrom(next.handle())
+      return { message: "success", data: res }
+    }catch(err){
+      return { message: "failed", data: err.message || ''}
+    }
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new DataWrapInterceptor())
   await app.listen(3000);
 }
 bootstrap();
