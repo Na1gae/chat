02a0e2529fc5c1bd52fb6ae003462b7daff36f81
Na1gae <nalgae@korea.ac.kr>
2024-08-25 22:42:42 +0900

    Feat: Datawrap Interceptor (주석처리함)

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { lastValueFrom } from 'rxjs';
-
+import { map, catchError, Observable, throwError } from 'rxjs';
+/*
 @Injectable()
-export class DataWrapInterceptor implements NestInterceptor{
-  async intercept(context: ExecutionContext, next: CallHandler): Promise<any>{
-    try{
-      const res = await lastValueFrom(next.handle())
-      return { message: "success", data: res }
-    }catch(err){
-      return { message: "failed", data: err.message || ''}
-    }
+export class DataWrapperInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map(data => ({
+        message: 'Request was successful',
+        data: data,
+      })),
+      catchError(err =>
+        throwError(() => ({
+          message: 'Request failed',
+          error: err.message || 'An error occurred',
+        })),
+      ),
+    );
   }
-}
+}*/
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new DataWrapInterceptor())
+  //app.useGlobalInterceptors(new DataWrapperInterceptor())
   await app.listen(3000);
 }
 bootstrap();
