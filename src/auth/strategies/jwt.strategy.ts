556309c353e863c8b5c155eb367d1ef1ef31c8ab
Na1gae <nalgae@korea.ac.kr>
2024-08-25 22:22:46 +0900

    !Fix: JWT

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy} from 'passport-jwt'
@@ -7,12 +7,18 @@ import { JwtPayload } from "./jwt-payload.interface";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy){
-    constructor(private configService: ConfigService, private userService: UserService) {
+    constructor(private readonly configService: ConfigService, private readonly userService: UserService) {
+        const secret = configService.get<string>('JWT_SECRET')
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: configService.get('JWT_SECRET_KEY')
+            secretOrKey: "H1k$8f2n0!z7jG5Q@Kx&V9pLqD3mN6uT^X8wR0yE7oB2sF1cI4rA"
         })
     }
 
+    async validate(payload: JwtPayload){
+        const user = await this.userService.getUserObjId(payload.userId)
+        if(!user) throw new UnauthorizedException()
+        return user
+    }
 }
